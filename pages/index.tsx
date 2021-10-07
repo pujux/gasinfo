7f56d597e13dec8e3c5303fdd618c151e7e3dff9
Julian Pufler <julian.pufler@lean-coders.at>
2021-10-07 17:04:22 +0200

    first version

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,21 @@ import { useEffect, useRef, useState } from "react";
 import { setInterval, setTimeout } from "timers";
 import Layout from "../components/layout";
 import { Line } from "react-chartjs-2";
+import Timespan from "readable-timespan";
+
+const { parse } = new Timespan();
 
 const Home = () => {
   let gasInterval,
     priceInterval,
     blocks = [];
   const [gasData, setGasData] = useState({
-    safeLow: NaN,
-    standard: NaN,
-    fast: NaN,
-    fastest: NaN,
-    blockTime: NaN,
-    blockNumber: NaN,
+    fastGasPrice: NaN,
+    lastBlock: NaN,
+    proposeGasPrice: NaN,
+    safeGasPrice: NaN,
+    gasUsedRatio: "",
+    suggestBaseFee: NaN,
   });
   const [price, setPrice] = useState(NaN);
   const [lastBlocks, setLastBlocks] = useState([]);
@@ -24,12 +27,12 @@ const Home = () => {
     if (priceInterval || gasInterval)
       return console.log("already defined intervals");
     fetchGasstation();
-    fetchMaticPrice();
+    fetchEthereumPrice();
 
-    gasInterval = setInterval(fetchGasstation, 15e2);
+    gasInterval = setInterval(fetchGasstation, 5e2);
     backgroundAnimateXRef.current.classList.add("refreshingX");
     backgroundAnimateYRef.current.classList.add("refreshingY");
-    priceInterval = setInterval(fetchMaticPrice, 12e4);
+    priceInterval = setInterval(fetchEthereumPrice, 12e4);
     return () => {
       clearInterval(gasInterval);
       clearInterval(priceInterval);
@@ -37,37 +40,65 @@ const Home = () => {
   }, []);
 
   const fetchGasstation = async () => {
-    const data = await fetch("https://gasstation-mainnet.matic.network")
+    const { result } = await fetch(
+      "https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=B38IETTAG8C6WMXV6AYWA8QWHCYKVNGB5U"
+    )
       .then((res) => res.json())
       .catch((err) => console.error("(╯°□°)╯︵ ┻━┻", err));
-    if (data && !blocks.find((b) => b.blockNumber === data.blockNumber)) {
+    if (result && !blocks.find((b) => b.lastBlock == result.LastBlock)) {
+      const data = {
+        fastGasPrice: parseFloat(result.FastGasPrice),
+        lastBlock: parseFloat(result.LastBlock),
+        proposeGasPrice: parseFloat(result.ProposeGasPrice),
+        safeGasPrice: parseFloat(result.SafeGasPrice),
+        gasUsedRatio: result.gasUsedRatio,
+        suggestBaseFee: parseFloat(result.suggestBaseFee),
+      };
+      const timeData = (
+        await Promise.all(
+          [data.fastGasPrice, data.proposeGasPrice, data.safeGasPrice].map(
+            (gwei) =>
+              fetch(
+                `https://api.etherscan.io/api?module=gastracker&action=gasestimate&gasprice=${
+                  gwei * 1e9
+                }&apikey=B38IETTAG8C6WMXV6AYWA8QWHCYKVNGB5U`
+              )
+                .then((res) => res.json())
+                .catch((err) => console.error("(╯°□°)╯︵ ┻━┻", err))
+          )
+        )
+      ).map(({ result }) => (typeof result === "number" ? result : null));
+      data.timeEstimates = timeData;
       blocks = [...blocks.slice(-24), { ...data, receivedAt: new Date() }];
       setLastBlocks(blocks);
       setGasData(data);
     }
   };
 
-  const fetchMaticPrice = async () => {
+  const fetchEthereumPrice = async () => {
     const response = await fetch(
-      "https://api.coingecko.com/api/v3/simple/price?ids=matic-network&vs_currencies=usd"
+      "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
     )
       .then((res) => res.json())
       .catch((err) => console.error("(╯°□°)╯︵ ┻━┻", err));
-    if (response["matic-network"]?.usd) setPrice(response["matic-network"].usd);
+    if (response["ethereum"]?.usd) setPrice(response["ethereum"].usd);
   };
 
   return (
     <div className="bg-primaryBackgroundLight dark:bg-primaryBackgroundDark">
       <Layout
         title={
-          !gasData.fastest ||
-          !gasData.fast ||
-          isNaN(gasData.fastest) ||
-          isNaN(gasData.fast)
+          !gasData.fastGasPrice ||
+          !gasData.proposeGasPrice ||
+          isNaN(gasData.fastGasPrice) ||
+          isNaN(gasData.proposeGasPrice)
             ? undefined
-            : gasData.fastest.toFixed(0) === gasData.fast.toFixed(0)
-            ? `${gasData.fast.toFixed(0)} Gwei`
-            : `${gasData.fastest.toFixed(0)}-${gasData.fast.toFixed(0)} Gwei`
+            : gasData.fastGasPrice.toFixed(0) ===
+              gasData.proposeGasPrice.toFixed(0)
+            ? `${gasData.proposeGasPrice.toFixed(0)} Gwei`
+            : `${gasData.fastGasPrice.toFixed(
+                0
+              )}-${gasData.proposeGasPrice.toFixed(0)} Gwei`
         }
       >
         <div className="mx-4 mb-16 md:mx-0">
@@ -77,19 +108,19 @@ const Home = () => {
                 Gas Price (Gwei)
               </h1>
               <h2 className="md:text-center text-md text-secondaryTextLight dark:text-secondaryTextDark">
-                Recommended Gas Price after analyzing the last 500 Transactions
+                Recommended Gas Price and Info
               </h2>
             </div>
-            <div className="grid grid-cols-1 gap-6 mb-4 md:gap-12 md:grid-cols-4">
+            <div className="grid grid-cols-1 gap-6 mb-4 md:gap-12 md:grid-cols-3">
               <div className="flex items-center justify-between p-4 overflow-hidden border border-solid md:flex-col rounded-xl border-tertiaryBackgroundLight dark:border-tertiaryBackgroundDark bg-secondaryTextDark dark:bg-secondaryTextLight">
                 <h2 className="w-10 text-lg font-bold md:text-xl md:w-auto text-primaryTextLight dark:text-primaryTextDark">
                   Rapid
                 </h2>
                 <h2 className="text-2xl font-bold text-green-500 md:text-5xl md:my-4">
-                  {gasData.fastest?.toFixed(0)}
+                  {gasData.fastGasPrice?.toFixed(0)}
                 </h2>
                 <h2 className="text-sm font-bold md:text-md text-secondaryTextLight dark:text-secondaryTextDark">
-                  ${((gasData.fastest * price) / 1e9).toFixed(10)}
+                  ${((gasData.fastGasPrice * price) / 1e9).toFixed(10)}
                 </h2>
               </div>
               <div className="relative flex items-center justify-between p-4 overflow-hidden border border-solid md:flex-col rounded-xl border-accentText bg-secondaryTextDark dark:bg-secondaryTextLight">
@@ -105,10 +136,10 @@ const Home = () => {
                   Fast
                 </h2>
                 <h2 className="z-10 text-2xl font-bold md:text-5xl text-accentText md:my-4">
-                  {gasData.fast?.toFixed(0)}
+                  {gasData.proposeGasPrice?.toFixed(0)}
                 </h2>
                 <h2 className="z-10 text-sm font-bold md:text-md text-secondaryTextLight dark:text-secondaryTextDark">
-                  ${((gasData.fast * price) / 1e9).toFixed(10)}
+                  ${((gasData.proposeGasPrice * price) / 1e9).toFixed(10)}
                 </h2>
               </div>
               <div className="flex items-center justify-between p-4 overflow-hidden border border-solid md:flex-col rounded-xl border-secondaryTextLight dark:border-secondaryTextDark bg-secondaryTextDark dark:bg-secondaryTextLight">
@@ -116,36 +147,25 @@ const Home = () => {
                   Standard
                 </h2>
                 <h2 className="text-2xl font-bold text-blue-600 md:text-5xl md:my-4">
-                  {gasData.standard?.toFixed(0)}
+                  {gasData.safeGasPrice?.toFixed(0)}
                 </h2>
                 <h2 className="text-sm font-bold md:text-md text-secondaryTextLight dark:text-secondaryTextDark">
-                  ${((gasData.standard * price) / 1e9).toFixed(10)}
-                </h2>
-              </div>
-              <div className="flex items-center justify-between p-4 overflow-hidden border border-solid md:flex-col rounded-xl border-secondaryTextLight dark:border-secondaryTextDark bg-secondaryTextDark dark:bg-secondaryTextLight">
-                <h2 className="w-10 text-lg font-bold md:text-xl md:w-auto text-primaryTextLight dark:text-primaryTextDark">
-                  Slow
-                </h2>
-                <h2 className="text-2xl font-bold text-purple-500 md:text-5xl md:my-4">
-                  {gasData.safeLow?.toFixed(0)}
-                </h2>
-                <h2 className="text-sm font-bold md:text-md text-secondaryTextLight dark:text-secondaryTextDark">
-                  ${((gasData.safeLow * price) / 1e9).toFixed(10)}
+                  ${((gasData.safeGasPrice * price) / 1e9).toFixed(10)}
                 </h2>
               </div>
             </div>
             <h1 className="text-md text-secondaryTextLight dark:text-secondaryTextDark">
-              MATIC: ${price}
+              Ethereum: ${price}
             </h1>
             <h1 className="text-md text-secondaryTextLight dark:text-secondaryTextDark">
-              Block: #{gasData.blockNumber}
+              Block: #{gasData.lastBlock}
             </h1>
           </div>
         </div>
-        <div className="max-w-4xl mx-auto mb-16">
+        <div className="hidden max-w-4xl mx-auto mb-16">
           <a href="https://gravityfinance.io">
             <img
-              className="hidden m-auto sm:block"
+              className="m-auto sm:block"
               src="/banner.gif"
               alt="Gravity Finance Banner"
             />
@@ -170,7 +190,9 @@ const Home = () => {
                 datasets: [
                   {
                     label: "Rapid",
-                    data: lastBlocks.map(({ fastest }) => fastest?.toFixed(0)),
+                    data: lastBlocks.map(({ fastGasPrice }) =>
+                      fastGasPrice?.toFixed(0)
+                    ),
                     fill: false,
                     backgroundColor: "#10b981",
                     borderColor: "#10b981",
@@ -179,7 +201,9 @@ const Home = () => {
                   },
                   {
                     label: "Fast",
-                    data: lastBlocks.map(({ fast }) => fast?.toFixed(0)),
+                    data: lastBlocks.map(({ proposeGasPrice }) =>
+                      proposeGasPrice?.toFixed(0)
+                    ),
                     fill: false,
                     backgroundColor: "#ffa600",
                     borderColor: "#ffa600",
@@ -188,8 +212,8 @@ const Home = () => {
                   },
                   {
                     label: "Standard",
-                    data: lastBlocks.map(({ standard }) =>
-                      standard?.toFixed(0)
+                    data: lastBlocks.map(({ safeGasPrice }) =>
+                      safeGasPrice?.toFixed(0)
                     ),
                     fill: false,
                     backgroundColor: "#2563eb",
